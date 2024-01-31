f139a010410c0a50c09626ef4e3289b4d4469469
seekpan <lixin_qiu@163.com>
2024-01-31 22:51:15 +0800

    优化测试ESAPI的使用代码

--- testforwork/src/main/java/com/test/work/test4work/controller/DirectionController.java
+++ testforwork/src/main/java/com/test/work/test4work/controller/DirectionController.java
@@ -2,10 +2,7 @@ package com.test.work.test4work.controller;
 
 import com.test.work.test4work.utils.DefaultUnixCodec;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.logging.log4j.util.PropertiesUtil;
 import org.owasp.esapi.ESAPI;
-import org.owasp.esapi.codecs.UnixCodec;
-import org.owasp.esapi.codecs.WindowsCodec;
 import org.owasp.esapi.errors.ValidationException;
 import org.owasp.esapi.reference.DefaultEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -25,56 +22,79 @@ import java.util.List;
 public class DirectionController {
 
     @GetMapping("/list")
-    public String getAllDirList(@RequestParam String path) throws IOException, InterruptedException, ValidationException {
-//        UnixCodec unixCodec = new UnixCodec();
-//
-//        path = DefaultEncoder.getInstance().encodeForOS(unixCodec, path);
-//        log.info("secure command -> " + path);
-//
-//        Runtime runtime = Runtime.getRuntime();
-//        Process process = runtime.exec(new String[]{"/bin/sh", "-c", "ls " + path + " -l | grep ^d | awk '{print $9}'"});
-////        Process process = runtime.exec(new String[]{"cmd.exe", "/c", "dir " + path});
-//        // 获取流
-//        InputStream inputStream = process.getInputStream();
-//        InputStream errorStream = process.getErrorStream();
-//
-//        InputStreamReader errorReader = new InputStreamReader(errorStream, Charset.forName("gbk"));
-//        InputStreamReader reader = new InputStreamReader(inputStream, Charset.forName("gbk"));
-//        // 读取
-//        int len;
-//        char[] buffer = new char[1024];
-//        // rs
-//        StringBuffer result = new StringBuffer();
-//        List<String> jsonResult = new ArrayList<>();
-//        while ((len = errorReader.read(buffer)) != -1) {
-//            String b = new String(buffer, 0, len);
-//            result.append(b);
-//            log.info(b);
-//            jsonResult.add(b);
-//        }
-//
-//        // 读取
-//        buffer = new char[1024];
-//
-//        while ((len = reader.read(buffer)) != -1) {
-//            String b = new String(buffer, 0, len);
-//            result.append(b);
-//            log.info(b);
-//            jsonResult.add(b);
-//        }
-//        inputStream.close();
-//        // 等待执行完成
-//        int exitVal = process.waitFor();
-//        log.info("process exit value is " + exitVal);
+    public String getAllDirList(@RequestParam String path) throws IOException, InterruptedException {
+
+        return testEncodeForOS(path);
+    }
+
+    /**
+     * 测试encodeForOS
+     *
+     * @param path
+     * @return
+     * @throws IOException
+     * @throws InterruptedException
+     */
+    private String testEncodeForOS(String path) throws IOException, InterruptedException {
+
+        path = DefaultEncoder.getInstance().encodeForOS(new DefaultUnixCodec(), path);
+        log.info("secure command -> " + path);
+
+        Runtime runtime = Runtime.getRuntime();
+        Process process = runtime.exec(new String[]{"/bin/sh", "-c", "ls " + path + " -l | grep ^d | awk '{print $9}'"});
+//        Process process = runtime.exec(new String[]{"cmd.exe", "/c", "dir " + path});
+        // 获取流
+        InputStream inputStream = process.getInputStream();
+        InputStream errorStream = process.getErrorStream();
+
+        InputStreamReader errorReader = new InputStreamReader(errorStream, Charset.forName("gbk"));
+        InputStreamReader reader = new InputStreamReader(inputStream, Charset.forName("gbk"));
+        // 读取
+        int len;
+        char[] buffer = new char[1024];
+        // rs
+        StringBuffer result = new StringBuffer();
+        List<String> jsonResult = new ArrayList<>();
+        while ((len = errorReader.read(buffer)) != -1) {
+            String b = new String(buffer, 0, len);
+            result.append(b);
+            log.info(b);
+            jsonResult.add(b);
+        }
+
+        // 读取
+        buffer = new char[1024];
+
+        while ((len = reader.read(buffer)) != -1) {
+            String b = new String(buffer, 0, len);
+            result.append(b);
+            log.info(b);
+            jsonResult.add(b);
+        }
+        inputStream.close();
+        // 等待执行完成
+        int exitVal = process.waitFor();
+        log.info("process exit value is " + exitVal);
+
+        return "success";
+    }
+
+    /**
+     * 测试getValidDirectoryPath
+     *
+     * @param path
+     * @return
+     * @throws ValidationException
+     */
+    private String testGetValidDirectoryPath(String path) throws ValidationException {
 
         File file = new File(path);
         if (file.exists()) {
-            System.out.println("origin存在");
+            System.out.println("origin path 存在");
         } else {
-            System.out.println("origin不存在");
+            System.out.println("origin path 不存在");
         }
 
-//        path = DefaultEncoder.getInstance().encodeForOS(new DefaultUnixCodec(), path);
         String validDirectoryPath = ESAPI.validator().getValidDirectoryPath(path, path,
                 new File("/"), false);
 
@@ -82,11 +102,11 @@ public class DirectionController {
 
         File file2 = new File(validDirectoryPath);
         if (file2.exists()) {
-            System.out.println("valid存在");
+            System.out.println("valid path 存在");
         } else {
-            System.out.println("valid不存在");
+            System.out.println("valid path 不存在");
         }
 
-        return "";
+        return "success";
     }
 }
--- testforwork/src/main/java/com/test/work/test4work/controller/TestController.java
+++ testforwork/src/main/java/com/test/work/test4work/controller/TestController.java
@@ -2,9 +2,11 @@ package com.test.work.test4work.controller;
 
 import com.test.work.test4work.utils.DefaultUnixCodec;
 import com.test.work.test4work.utils.DefaultWindowCodec;
+import org.owasp.esapi.ESAPI;
 import org.owasp.esapi.codecs.UnixCodec;
 import org.owasp.esapi.codecs.WindowsCodec;
 import org.owasp.esapi.errors.EncodingException;
+import org.owasp.esapi.errors.ValidationException;
 import org.owasp.esapi.reference.DefaultEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,24 +21,13 @@ public class TestController {
         return "test";
     }
 
-    public static void main(String[] args) throws EncodingException {
-//        String path = "/com/test_?/..";
-//        UnixCodec unixCodec = new UnixCodec();
-//        char[] chars = path.toCharArray();
-//        String result = "";
-//        for (Character c : chars) {
-//            String s = unixCodec.encodeCharacter(new char[]{'*'}, c);
-//            result += s;
-//        }
-//        System.out.println("去除*:" + result);
+    public static void main(String[] args) throws ValidationException {
 
-//        path = DefaultEncoder.getInstance().encodeForOS(new DefaultUnixCodec(), path);
-//        System.out.println(path);
+        String filePath = "D:" + File.separator + "sofaware";
+        String validDirectoryPath = ESAPI.validator().getValidDirectoryPath(filePath, filePath,
+                new File("D:\\sofaware"), false);
 
-        String filePath = "D:" + File.separator + "sofaware" + File.separator + "acrobat_225409.rar";
-        filePath = DefaultEncoder.getInstance().encodeForURL(filePath);
-
-        File file = new File(filePath);
+        File file = new File(validDirectoryPath);
         if (file.exists()) {
             System.out.println("存在");
         } else {
